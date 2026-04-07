f70fcaed4f8a0073b3a866267a7a381f0dbcdeca
Nikhil-n17 <56041789+Nikhil-n17@users.noreply.github.com>
2026-04-07 22:25:21 +0530

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.java
@@ -0,0 +1,80 @@
+class Solution {
+    private boolean freshOrangeExist(int[][] grid, int row, int col) {
+        for(int i = 0 ; i < row ; i++) {
+            for(int j = 0 ; j < col ; j++) {
+                if(grid[i][j] == 1) return true;
+            }
+        }
+        return false;
+    }
+    private int getRottenOrangesCount(int[][] grid, int row, int col) {
+        int count = 0;
+        for(int i = 0 ; i < row ; i++) {
+            for(int j = 0 ; j < col ; j++) {
+                if(grid[i][j] == 2) count+=1;
+            }
+        }
+        return count;
+    }
+    private List<int[]> updateAndReturnRottenIndices(int[][] grid, List<int[]> rottenIndices, int row, int col) {
+        List<int[]> affectedIndices = new ArrayList<>();
+        for(int[] index: rottenIndices) {
+            int aRow = index[0];
+            int aCol = index[1];
+            if(aRow + 1 < row) {
+                if(grid[aRow+1][aCol]== 1) {
+                    grid[aRow+1][aCol] = 2;
+                    affectedIndices.add(new int[] {aRow+1, aCol});
+                }
+            }
+            if(aRow - 1 >= 0) {
+                if(grid[aRow-1][aCol] == 1) {
+                    grid[aRow-1][aCol] = 2;
+                    affectedIndices.add(new int[] {aRow-1, aCol});
+                }
+            }
+            if(aCol + 1 < col) {
+                if(grid[aRow][aCol+1]== 1) {
+                    grid[aRow][aCol+1] = 2;
+                    affectedIndices.add(new int[] {aRow, aCol+1});
+                }
+            }
+            if(aCol - 1 >= 0) {
+                if(grid[aRow][aCol-1]==1) {
+                    grid[aRow][aCol-1] = 2;
+                    affectedIndices.add(new int[] {aRow, aCol-1});
+                }
+            }
+        }
+        return affectedIndices;
+    }
+    private List<int[]> getRottenIndicesInitially(int[][] grid, int row, int col) {
+        List<int[]> affectedIndices = new ArrayList<>();
+        for(int i = 0 ; i < row ; i++) {
+            for(int j = 0 ; j < col ; j++) {
+                if(grid[i][j] == 2) affectedIndices.add(new int[]{i,j});
+            }
+        }
+        return affectedIndices;
+    }
+    private void printIndices(int[][] grid, List<int[]> indices) {
+        for(int[] index : indices) {
+            System.out.printf("%d %d\n", index[0], index[1]);
+        }
+        System.out.println("Completed");
+    }
+    public int orangesRotting(int[][] grid) {
+        int res = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        List<int[]> affectedIndices = getRottenIndicesInitially(grid,row,col);
+        while(freshOrangeExist(grid, row, col)) {
+            int prev = getRottenOrangesCount(grid, row,col);
+            affectedIndices = updateAndReturnRottenIndices(grid, affectedIndices, row, col);
+            int after = getRottenOrangesCount(grid, row,col);
+            if(prev == after) return -1;
+            res+=1;
+        }
+        return res;
+    }
+}
